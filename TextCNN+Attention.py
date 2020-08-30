cbf315145fedff94b5751ef595df8f327f560a16
WuYiCHEN <40305074+WuYiJhen@users.noreply.github.com>
2020-08-30 21:18:53 +0800

    Update TextCNN+Attention.py

--- TextCNN+Attention.py
+++ TextCNN+Attention.py
@@ -24,30 +24,19 @@ class TextCNN(nn.Module):
         self.embed_drop = nn.Dropout(p = self.embed_drop)
         self.convs = nn.ModuleList([nn.Conv2d(1, self.num_filter, (k, self.embedding_dim)) for k in self.kernel])
         
-        self.U = nn.ModuleList([nn.Linear(self.Y, self.num_filter) for k in self.kernel]) # [C, Y]
-        self.W1 = nn.Linear(self.num_filter//2, self.num_filter) # [C, C//2]
-        self.B1 = nn.Linear(self.num_filter//2, self.Y * len(self.kernel)) # [len(kernel) * Y, C//2]
-        self.W2 = nn.Linear(1, self.num_filter//2) # [C//2, 1]
-        self.B2 = nn.Linear(1, self.Y * len(self.kernel)) # [len(kernel) * Y, 1]
-        self.fc = nn.Linear(self.Y * len(self.kernel), self.Y)
-        self.fco = nn.Linear(self.num_filter * len(self.kernel), self.Y)
-        
         for conv in self.convs:
-            xavier_uniform(conv.weight) 
+            xavier_uniform(conv.weight)
         
-        for u in self.U:
-            xavier_uniform(u.weight)
-        
-        xavier_uniform(self.W1.weight)
-        xavier_uniform(self.B1.weight)
-
-#         self.W = nn.ModuleList([nn.Linear(self.num_filter//2, self.num_filter) for k in self.kernel]) # [C, C//2]
-#         self.B = nn.ModuleList([nn.Linear(self.num_filter//2, self.Y) for k in self.kernel]) # [Y, C//2]
-#         for u, w, b in (self.U, self.W, self.B):
-#             xavier_uniform(U.weight)
-#             xavier_uniform(w.weight)
-#             xavier_uniform(b.weight)
-                               
+        if self.Attention:
+            self.U = nn.ModuleList([nn.Linear(self.Y, self.num_filter) for k in self.kernel]) # [C, Y]
+            for u in self.U:
+                xavier_uniform(u.weight)
+            self.l1 = nn.Linear(self.num_filter, self.num_filter//2)
+            self.l2 = nn.Linear(self.num_filter//2, 1)
+            self.fc = nn.Linear(self.Y * len(self.kernel), self.Y)
+        else:
+            self.fc = nn.Linear(self.num_filter * len(self.kernel), self.Y)
+                                       
     def forward(self, x):
         
         # B: Batch Size
@@ -74,22 +63,21 @@ class TextCNN(nn.Module):
             x = torch.cat([torch.matmul(x[i], alphas[i]) for i in range(len(self.convs))], 2)
             # [B, C, len(kernel) * Y]
 
-            x = F.relu(torch.add(torch.matmul(x.transpose(1, 2), self.W1.weight), self.B1.bias.unsqueeze(1)))
+            x = F.relu(self.l1(x.transpose(1, 2)))
             # [B, len(kernel) * Y, C//2]
-
-            x = torch.add(torch.matmul(x, self.W2.weight), self.B2.bias.unsqueeze(1))
+            
+            x = self.l2(x)
             # [B, len(kernel) * Y, 1]
 
             x = self.fc(x.squeeze(2))
             # [B, Y]
-
-    #         x = [torch.add(torch.matmul(x[i].transpose(1, 2), self.W[i].weight), self.B[i].bias.unsqueeze(1)) for i in range(len(self.convs))]
+            
         else:
             x = [F.max_pool1d(conv(x).squeeze(-1), conv(x).size()[2]) for conv in self.convs]
             # [B, C, 1]
             x = torch.cat(x, 1).squeeze(-1)
             # [B, C * len(kernel)]
-            x = self.fco(x)
+            x = self.fc(x)
             # [B, Y]
             
         if self.Attention: return x, alphas
